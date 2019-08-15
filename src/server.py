f5f3ce52c002fd327f1ebc7cd6550914977c824e
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-15 13:50:02 -0700

    Modified server.py to include route for homepage, and added post request to retrieve document info. Need to add functionality to load info into db

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from Jinja2 import StrictUndefined
+from jinja2 import StrictUndefined
 
 from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
@@ -12,15 +12,41 @@ app.secret_key = "ABC"
 
 # mocking out server.py for now
 
-@app.route('/document')
+@app.route('/')
+def display_homepage():
+    """ Displays homepage """
+
+    return render_template('homepage.html')
+
+
+@app.route('/upload_file')
 def upload_document():
     """ Allows user to upload a document """
 
-    return render_template("upload_document.html")
+    return render_template("upload_file.html")
 
 
-@app.route('/document/{id}')
+@app.route('/file_view', methods=['POST'])
 def display_document():
     """ Displays the document of user's choice """
 
-    return render_template("document_{id}.html")
+    file = request.form.get('file')
+    filename = request.form.get('filename')
+
+    print(filename)
+    print(file)
+
+
+    return render_template("file_view.html", filename=filename, file=file)
+
+
+if __name__ == "__main__":
+
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
