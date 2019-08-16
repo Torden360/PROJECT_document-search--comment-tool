0a3828ff7cbaf99e51143fcba149249efe56cd89
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-16 16:28:58 -0700

    Changed column type from db.Text to db.LargeBinary to store complete text file

--- src/model.py
+++ src/model.py
@@ -11,47 +11,13 @@ class Document(db.Model):
     __tablename__ = "documents"
 
     document_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
+    text = db.Column(db.LargeBinary, nullable=False)
+    # db.LargeBinary stores the file in a bytea column, sqlalchemy takes care of conversion
     name = db.Column(db.String(60))
 
     def __repr__(self):
 
         return f'<document_id = {self.document_id} name = {self.name}>'
-
-
-    def load_text(self, document, name):
-        """ Load the text from the document into database """
-
-    # Document.query.delete()
-    # delete existing documents so we don't get an error when running this file
-    # multiple times
-    # on second thought, I don't think I'll run this file as itself, but import 
-    # FNs to other files from it
-
-        document.name = name 
-
-        print(document.name)
-
-        # document = open(document)
-        # I swear there's another way to open a file without having to close it--have
-        # to look for that tonight. 
-
-        # name = (document)
-        # Having user input name
-        
-        text = document.read()
-
-        # document = Document(name=name, 
-        #                     text=text
-        #                     )
-
-        document.text = text
-
-        db.session.add(document)
-
-        db.session.commit()
-
-        # document.close()
 
 
 class Search(db.Model):
