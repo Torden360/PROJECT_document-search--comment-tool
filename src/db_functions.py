7517a37f75d9618fa0258fbc27ad84b181c87370
Hackbright Student <no-reply@hackbrightacademy.com>
2019-09-03 16:27:02 -0700

    Created FNs create_group, store_match, and store_notes, updated store_search FN to return search_id

--- src/db_functions.py
+++ src/db_functions.py
@@ -1,4 +1,4 @@
-from model import (Document, Search, Search_Match, connect_to_db, db) 
+from model import (Document, Search, Search_Match, Group, Note, connect_to_db, db) 
 
 def load_text(document, name):
         """ Load the text from the document into database, and create new db object """
@@ -26,13 +26,37 @@ def store_search(search_phrase, document_id):
     db.session.add(search)
     db.session.commit()
 
+    return search.search_id
 
-def store_match(search_id, start_offset, end_offset):
+
+def create_group(search_id):
+
+    group = Group(search_id=search_id)
+
+    db.session.add(group)
+    db.session.commit()
+
+    return group.group_id
+
+
+def store_match(search_id, start_offset, end_offset, match_content):
 
     match = Search_Match(search_id=search_id,
                   start_offset=start_offset,
-                  end_offset=end_offset)
+                  end_offset=end_offset,
+                  match_content=match_content)
 
     db.session.add(match)
     db.session.commit()
 
+    return match.match_id
+
+
+def store_notes(note_content, match_id, group_id):
+
+    note = Note(note_content=note_content,
+                match_id=match_id,
+                group_id=group_id)
+
+    db.session.add(note)
+    db.session.commit()
