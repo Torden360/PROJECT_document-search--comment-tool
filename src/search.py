bce1670acdc02f89344c4a6325020e21fef5f094
Tyler Ordenstein <tylerordenstein@gmail.com>
2019-09-15 23:46:06 -0700

    Changed from tuples to lists for JS to parse correctly

--- src/search.py
+++ src/search.py
@@ -33,12 +33,17 @@ def get_location(phrase):
 
 
 def get_context(phrase, document_text):
-
-    matches = re.finditer(r'[^.?!]*[.?!(?:\s)]*[^.?!]*\b({})\b[^.?!]*[.?!(?:\s)]*[^.?!]*[.?!(?:\s)]'.format(phrase), document_text, re.IGNORECASE)
+# why doesn't this work? compared to def search I know why. it's because the . is not the special char, when it's in a group like that,
+# it's escaped
+    matches = re.finditer(r'[^.]*[.(?:\s)]*[^.]*(\b{}\b)[^.]*[.(?:\s)]*[^.]*[.(?:\s)]'.format(phrase), document_text, re.IGNORECASE)
     # this works great! except, if the word match is in the same sentence, it only returns one
     # match/comment box. And, seems like a lot of work/takes a long time in a large file like Hamlet
-    print(matches.values())
+    # print(matches.values())
     # print(matches.end)
+
+    for match in matches:
+        print('match.groups: ', match.groups())
+        print('match.group: ', match.group(), match.start(), match.end()) #, document_text[int(f'{match.start})':int(f'{match.end}')])
     return matches
 
 
@@ -46,9 +51,30 @@ def search(phrase, document_text):
 
 #     matches = re.findall(r'\b{}\b'.format(phrase), document_text)
 
-    matches = re.findall(r'[^.?!]*[.?!(?:\s)]*[^.?!]*\b{}\b[^.?!]*[.?!(?:\s)]*[^.?!]*[.?!(?:\s)]'.format(phrase), document_text, re.IGNORECASE)
+    matches = re.finditer(r'[^.?!]*[.?!(?:\s)]*[^.?!]*\b({})\b[^.?!]*[.?!(?:\s)]*[^.?!]*[.?!(?:\s)]'.format(phrase), document_text, re.IGNORECASE)
+    print('this be matches:', matches)
+    matches_list = []
+    for match in matches:
+        print('this be match', match)
+        print(match.start(1), match.end(1), 'trying to get diff index', match.end())
+        print(match.span(1), 'we shall see')
+        match_dict = {}
+        match_gr = match.group()
+        match_dict['match'] = match_gr.replace('\n', ' ')
+        match_dict['start'] = [match.start(1), match.start()]
+        match_dict['end'] = [match.end(1), match.end()]
+        matches_list.append(match_dict)
 
-    return matches
+    print(matches_list)
+    return matches_list
+
+    # This is not working properly, is not matching on last word tomorrow:
+    # search('hey', '''hey this is so much fun today i felt like. i just knew. there was going to be. a day someday somehwere. somehow. there's always a day awayyyyyyyy. tomorrow ! tomorrow ! i lov ya. hey, tomorrow''')
+    # this is match.groups:  ('hey',)
+    # this is match.group:  hey this is so much fun today i felt like. i just knew.
+    # this is match.groups:  ('hey',)
+    # this is match.group:   i lov ya. hey,
+    # <callable_iterator object at 0x10e336f98>
 
     # open_doc(document)
 
